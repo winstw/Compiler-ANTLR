87ca96152a44cc9ebd91e3a70e22804416a942cb
JB <jean-benoit.milbouw@student.unamur.be>
2020-03-08 21:13:40 +0100

    Expression tests

--- src/test/java/be/unamur/info/b314/compiler/main/SlipSyntaxExpressionTest.java
+++ src/test/java/be/unamur/info/b314/compiler/main/SlipSyntaxExpressionTest.java
@@ -28,8 +28,92 @@ public class SlipSyntaxExpressionTest {
 
     // tests OK
     @Test
-    public void test_ExpressionScalar_ok() throws Exception{
-        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionScalar.slip", testFolder.newFile(), true, "syntax::expression: ExpressionScalar.slip");
+    public void test_ExpressionIntegerSum_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionIntegerSum.slip", testFolder.newFile(), true, "syntax::expression: ExpressionIntegerSum.slip");
+    }
+
+
+    @Test
+    public void test_ExpressionBooleanNot_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionBooleanNot.slip", testFolder.newFile(), true, "syntax::expression: ExpressionBooleanNot.slip");
+    }
+
+
+    @Test
+    public void test_ExpressionBooleanAnd_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionBooleanAnd.slip", testFolder.newFile(), true, "syntax::expression: ExpressionBooleanAnd.slip");
+    }
+
+
+    @Test
+    public void test_ExpressionBooleanGreater_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionBooleanGreater.slip", testFolder.newFile(), true, "syntax::expression: ExpressionBooleanGreater.slip");
+    }
+
+
+    @Test
+    public void test_ExpressionBoolean_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionBoolean.slip", testFolder.newFile(), true, "syntax::expression: ExpressionBoolean.slip");
+    }
+
+
+    @Test
+    public void test_ExpressionBooleanEqual_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionBooleanEqual.slip", testFolder.newFile(), true, "syntax::expression: ExpressionBooleanEqual.slip");
+    }
+
+
+    @Test
+    public void test_ExpressionIntegerDivide_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionIntegerDivide.slip", testFolder.newFile(), true, "syntax::expression: ExpressionIntegerDivide.slip");
+    }
+
+
+    @Test
+    public void test_ExpressionBooleanDifferent_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionBooleanDifferent.slip", testFolder.newFile(), true, "syntax::expression: ExpressionBooleanDifferent.slip");
+    }
+
+
+    @Test
+    public void test_ExpressionBooleanLessEq_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionBooleanLessEq.slip", testFolder.newFile(), true, "syntax::expression: ExpressionBooleanLessEq.slip");
+    }
+
+
+    @Test
+    public void test_ExpressionIntegerOpposite_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionIntegerOpposite.slip", testFolder.newFile(), true, "syntax::expression: ExpressionIntegerOpposite.slip");
+    }
+
+
+    @Test
+    public void test_ExpressionInteger_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionInteger.slip", testFolder.newFile(), true, "syntax::expression: ExpressionInteger.slip");
+    }
+
+
+    @Test
+    public void test_ExpressionIntegerDifference_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionIntegerDifference.slip", testFolder.newFile(), true, "syntax::expression: ExpressionIntegerDifference.slip");
+    }
+
+
+    @Test
+    public void test_ExpressionIntegerProduct_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionIntegerProduct.slip", testFolder.newFile(), true, "syntax::expression: ExpressionIntegerProduct.slip");
+    }
+
+
+    @Test
+    public void test_ExpressionBooleanGreaterEq_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionBooleanGreaterEq.slip", testFolder.newFile(), true, "syntax::expression: ExpressionBooleanGreaterEq.slip");
+    }
+
+
+    @Test
+    public void test_ExpressionBooleanOr_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionBooleanOr.slip", testFolder.newFile(), true, "syntax::expression: ExpressionBooleanOr.slip");
     }
 
 
@@ -39,10 +123,22 @@ public class SlipSyntaxExpressionTest {
     }
 
 
+    @Test
+    public void test_ExpressionChar_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionChar.slip", testFolder.newFile(), true, "syntax::expression: ExpressionChar.slip");
+    }
+
+
+    @Test
+    public void test_ExpressionBooleanLess_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ok/ExpressionBooleanLess.slip", testFolder.newFile(), true, "syntax::expression: ExpressionBooleanLess.slip");
+    }
+
+
     // tests KO
     @Test
-    public void test_ExpressionBooleanKO_ko() throws Exception{
-        CompilerTestHelper.launchCompilation("/syntax/expression/ko/ExpressionBooleanKO.slip", testFolder.newFile(), false, "syntax::expression: ExpressionBooleanKO.slip");
+    public void test_ExpressionSymbolKO_ko() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/expression/ko/ExpressionSymbolKO.slip", testFolder.newFile(), false, "syntax::expression: ExpressionSymbolKO.slip");
     }
 
 

a7e281feb50759ff873cce04cab6f83fe6f28f53
enoirhom <emil.noirhomme@student.unamur.be>
2020-03-26 22:00:49 +0100

    change to make 1st phase define only global symbols

--- src/main/java/be/unamur/info/b314/compiler/main/GlobalDefinitionPhase.java
+++ src/main/java/be/unamur/info/b314/compiler/main/GlobalDefinitionPhase.java
@@ -16,7 +16,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 
-public class DefinitionPhaseVisitor extends SlipBaseVisitor<Integer> {
+public class GlobalDefinitionPhase extends SlipBaseVisitor<Integer> {
 
     public static void main(String[] args) throws IOException {
         File input = new File(System.getProperty("user.dir") + "/src/test/resources/DefPhaseTest.slip");
@@ -26,8 +26,12 @@ public class DefinitionPhaseVisitor extends SlipBaseVisitor<Integer> {
         parser.setErrorHandler(new SlipErrorStrategy());
 
         SlipParser.ProgramContext tree = parser.program();
-        DefinitionPhaseVisitor visitor = new DefinitionPhaseVisitor();
+        GlobalDefinitionPhase visitor = new GlobalDefinitionPhase();
         visitor.visit(tree);
+    }
+
+    public ParseTreeProperty<SlipScope> getScopes() {
+        return scopes;
     }
 
     private ParseTreeProperty<SlipScope> scopes = new ParseTreeProperty<>();
@@ -53,9 +57,14 @@ public class DefinitionPhaseVisitor extends SlipBaseVisitor<Integer> {
 
         List<SlipParser.VarDeclContext> vars = ctx.varDecl();
         List<SlipParser.FuncDeclContext> functs = ctx.funcDecl();
+        List<SlipParser.ConstDeclContext> consts = ctx.constDecl();
 
         for (SlipParser.VarDeclContext var : vars) {
             visit(var);
+        }
+
+        for (SlipParser.ConstDeclContext constant : consts) {
+            visit(constant);
         }
 
         for (SlipParser.FuncDeclContext func : functs) {
@@ -94,7 +103,6 @@ public class DefinitionPhaseVisitor extends SlipBaseVisitor<Integer> {
     /**
      * @modifies this, System.err
      * @effect add ctx to currentScope if it doesn't contain it, else print an error
-     * @return the SlipStructureScope added to currentScope
      */
     private void defineStructure(SlipParser.VarDefContext ctx) {
 
@@ -104,16 +112,11 @@ public class DefinitionPhaseVisitor extends SlipBaseVisitor<Integer> {
 
             try {
                 currentScope.define(symbol);
-                currentScope = symbol;
-                visit(ctx.type().structure());
-                System.out.println(currentScope);
-                currentScope = currentScope.getParentScope();
             } catch (NullPointerException e) {
                 e.printStackTrace();
             } catch (SymbolAlreadyDefinedException e) {
                 errorOccuried = true;
                 printError(node.getSymbol(), String.format("structure symbol \"%s\" already exists in %s scope", name, currentScope.getName()));
-
             }
 
         }
@@ -125,7 +128,6 @@ public class DefinitionPhaseVisitor extends SlipBaseVisitor<Integer> {
      * @effect add ctx to currentScope if it doesn't contain it, else print an error
      */
     private void defineVariable(SlipParser.VarDefContext ctx, SlipSymbol.Types type) {
-
         for (TerminalNode node : ctx.ID()) {
             String name = node.getText();
             SlipSymbol symbol = new SlipVariableSymbol(name, type);
@@ -146,14 +148,61 @@ public class DefinitionPhaseVisitor extends SlipBaseVisitor<Integer> {
     }
 
     @Override
-    public Integer visitStructure(SlipParser.StructureContext ctx) {
-        List<SlipParser.VarDeclContext> vars = ctx.varDecl();
+    public Integer visitConstDecl(SlipParser.ConstDeclContext ctx) {
 
-        for (SlipParser.VarDeclContext var : vars) {
-            visit(var);
+        SlipSymbol.Types type = getType(ctx.type().start.getType());
+
+        if (type == SlipSymbol.Types.STRUCT) {
+            defineConstantStructure(ctx);
+        } else {
+            defineConstant(ctx, type);
         }
 
         return 1;
+    }
+
+    /**
+     * @modifies this, System.err
+     * @effect add ctx to currentScope if it doesn't contain it, else print an error
+     */
+    private void defineConstantStructure(SlipParser.ConstDeclContext ctx) {
+
+            String name = ctx.ID().getText();
+            SlipStructureSymbol symbol = new SlipStructureSymbol(name, currentScope);
+
+            try {
+                currentScope.define(symbol);
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            } catch (SymbolAlreadyDefinedException e) {
+                errorOccuried = true;
+                printError(ctx.ID().getSymbol(), String.format("structure symbol \"%s\" already exists in %s scope", name, currentScope.getName()));
+
+            }
+
+    }
+
+    /**
+     * @modifies this, System.err
+     * @effect add ctx to currentScope if it doesn't contain it, else print an error
+     */
+    private void defineConstant(SlipParser.ConstDeclContext ctx, SlipSymbol.Types type) {
+
+        String name = ctx.ID().getText();
+        SlipSymbol symbol = new SlipVariableSymbol(name, type);
+
+        try {
+            currentScope.define(symbol);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (SymbolAlreadyDefinedException e) {
+            errorOccuried = true;
+            printError(ctx.ID().getSymbol(), String.format("variable symbol \"%s\" already exists in %s scope", name, currentScope.getName()));
+
+        }
+
+        System.out.println(symbol);
+
     }
 
     @Override
@@ -174,22 +223,6 @@ public class DefinitionPhaseVisitor extends SlipBaseVisitor<Integer> {
 
         try {
             currentScope.define(symbol);
-            currentScope = symbol;
-            scopes.put(ctx, symbol);
-
-            for (SlipParser.VarDefContext var : ctx.argList().varDef()) {
-                visit(var);
-            }
-
-            for (SlipParser.InstBlockContext instBlock : ctx.instBlock()) {
-                for (SlipParser.VarDeclContext var : instBlock.varDecl()) {
-                    visit(var);
-                }
-            }
-
-            System.out.println(currentScope);
-            currentScope = currentScope.getParentScope();
-
         } catch (NullPointerException e) {
             e.printStackTrace();
         } catch (SymbolAlreadyDefinedException e) {
@@ -205,17 +238,11 @@ public class DefinitionPhaseVisitor extends SlipBaseVisitor<Integer> {
 
         try {
             currentScope.define(symbol);
-            currentScope = symbol;
-            scopes.put(ctx, symbol);
-
-            System.out.println(currentScope);
-            currentScope = currentScope.getParentScope();
-
         } catch (NullPointerException e) {
             e.printStackTrace();
         } catch (SymbolAlreadyDefinedException e) {
             errorOccuried = true;
-            printError(ctx.getStart(), "function symbol already exists in " + currentScope.getName() + " scope");
+            printError(ctx.getStart(), "main symbol already exists in " + currentScope.getName() + " scope");
         }
 
         return 1;
